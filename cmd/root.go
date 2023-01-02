26d6b16a8b05829f343f5011e650ca5b2b217951
kumak1 <makino@pepabo.com>
2023-01-02 18:57:09 +0900

    add config

--- cmd/root.go
+++ cmd/root.go
@@ -23,18 +23,24 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Repos map[string]*RepositoryConfig
+}
+
+type RepositoryConfig struct {
+	Path    string
+	Depends []string
+}
+
 var cfgFile string
+
+var config Config
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "kcg",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "git command wrapper",
+	Long:  `This is git command wrapper CLI.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -84,5 +90,11 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+
+	// 設定ファイルの内容を構造体にコピーする
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
