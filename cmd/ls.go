b46bc0813810af53a786620a101168981043e67a
kumak1 <makino@pepabo.com>
2023-01-02 19:02:16 +0900

    ls で config にある repository 一覧を取得する処理を実装

--- cmd/ls.go
+++ cmd/ls.go
@@ -17,35 +17,28 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
+	"text/tabwriter"
 )
 
 // lsCmd represents the ls command
 var lsCmd = &cobra.Command{
 	Use:   "ls",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Show repository list.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ls called")
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+		fmt.Fprintln(w, "NAME\tPATH")
+
+		for index, repo := range config.Repos {
+			fmt.Fprintln(w, index+"\t"+repo.Path)
+		}
+
+		w.Flush()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(lsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
