28d473f65a906bac734e4fc0d64ca0eac8b2975b
kumak1 <makino@pepabo.com>
2023-01-03 21:20:33 +0900

    pull コマンドに repo オプションを追加

--- cmd/pull.go
+++ cmd/pull.go
@@ -24,17 +24,17 @@ import (
 // pullCmd represents the pull command
 var pullCmd = &cobra.Command{
 	Use:   "pull",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "run `git pull` on each repository dir",
+	Long:  `Running git pull command on each repository dir`,
 	Run: func(cmd *cobra.Command, args []string) {
+		repoFlag, _ := cmd.Flags().GetString("repo")
 		gitCommand := kcg.GitCommand(config)
 
-		for _, repo := range config.Repos {
+		for index, repo := range config.Repos {
+			if repoFlag != "" && repoFlag != index {
+				continue
+			}
+
 			err := gitCommand.Pull(repo)
 			if err != nil {
 				fmt.Println(err)
@@ -45,14 +45,5 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(pullCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// pullCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// pullCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	pullCmd.Flags().String("repo", "", "repository name")
 }
