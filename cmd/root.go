aac306f2a974e465b1f8f8ac5e60fe17be808d11
kumak1 <makino@pepabo.com>
2023-01-29 23:17:47 +0900

    config file の読み込みエラーハンドリングを追加

--- cmd/root.go
+++ cmd/root.go
@@ -89,8 +89,10 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		//fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("The Config file could not be loaded.")
+		os.Exit(1)
 	}
 
 	// 設定ファイルの内容を構造体にコピーする
