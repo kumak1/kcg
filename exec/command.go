c50d7103b3cc37ffd6ed24ef39a8a951c8bc2498
kumak1 <makino@pepabo.com>
2023-01-30 20:05:00 +0900

    kcg/exec の1箇所しか利用箇所のないメソッドを削除

--- exec/command.go
+++ exec/command.go
@@ -23,22 +23,6 @@ func New() Interface {
 }
 
 func (e defaultExec) FileExists(path string) bool {
-	return fileExists(path)
-}
-
-func (e defaultExec) DirExists(path string) bool {
-	return dirExists(path)
-}
-
-func (e defaultExec) Output(path string, name string, arg ...string) (string, error) {
-	return output(path, name, arg...)
-}
-
-func (e defaultExec) NotError(path string, name string, arg ...string) bool {
-	return notError(path, name, arg...)
-}
-
-func fileExists(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false
 	} else {
@@ -46,7 +30,7 @@ func fileExists(path string) bool {
 	}
 }
 
-func dirExists(path string) bool {
+func (e defaultExec) DirExists(path string) bool {
 	if f, err := os.Stat(path); os.IsNotExist(err) || !f.IsDir() {
 		return false
 	} else {
@@ -54,14 +38,14 @@ func dirExists(path string) bool {
 	}
 }
 
-func output(path string, name string, arg ...string) (string, error) {
+func (e defaultExec) Output(path string, name string, arg ...string) (string, error) {
 	cmd := exec.Command(name, arg...)
 	cmd.Dir = path
 	out, err := cmd.CombinedOutput()
 	return strings.TrimRight(string(out), "\n"), err
 }
 
-func notError(path string, name string, arg ...string) bool {
+func (e defaultExec) NotError(path string, name string, arg ...string) bool {
 	cmd := exec.Command(name, arg...)
 	cmd.Dir = path
 	return cmd.Run() == nil
